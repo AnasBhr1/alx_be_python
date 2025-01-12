147eea85e764201126230bfc67a8e4a848848f40
anas bouhrim <anasbhr1@hotmail.com>
2025-01-12 21:36:32 +0100

    Fix2: Correctly define global conversion factors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,7 @@
 # Global Conversion Factors
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  # Conversion factor from Fahrenheit to Celsius
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Conversion factor from Celsius to Fahrenheit
+FAHRENHEIT_OFFSET = 32
 
 def convert_to_celsius(fahrenheit):
     """
